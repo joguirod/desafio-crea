9713393b26acc9caee3f44ebe921bb196985f17f
Joguirod <joguicane@gmail.com>
2024-02-10 18:05:48 -0300

    starting project

---
+++ src/main/java/com/gm2/desafio/creaapi/CreaApiApplication.java
@@ -0,0 +1,13 @@
+package com.gm2.desafio.creaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gm2/desafio/creaapi/CreaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gm2.desafio.creaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
